396b786d00de3d00d2f9fd18d898e877abd61cca
Pranjal lab <pranjalkhatri950@gmail.com>
2025-09-05 17:08:53 +0530

    :]

--- lab1/performance/stest.cpp
+++ lab1/performance/stest.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
-#include <cstdlib>   // for system()
+#include <cstdlib> // for system()
 #include <string>
 
-int main() {
-    for (int i = 1; i <= 32; i++) {
-        std::string cmd = "./server tcp 6969 1 " + std::to_string(i) + " >> rs2.txt";
-        std::cout << "Executing: " << cmd << std::endl;
-        int ret = std::system(cmd.c_str());
-        if (ret != 0) {
-            std::cerr << "Command failed with return code " << ret << std::endl;
-            break;
+int main()
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        std::string tmp = "echo > rs"+std::to_string(j)+".txt";
+        system(tmp.c_str());
+        for (int i = 1; i <= 32; i++)
+        {
+            std::string cmd = "./server tcp 6969 1 " + std::to_string(i) + " >> rs3.txt";
+            std::cout << "Executing: " << cmd << std::endl;
+            int ret = std::system(cmd.c_str());
+            if (ret != 0)
+            {
+                std::cerr << "Command failed with return code " << ret << std::endl;
+                break;
+            }
         }
     }
     return 0;
